ce4591936b3a502909b226a0285dfed6452d919d
Nelida Valero <nelidavalero06@gmail.com>
2021-03-04 09:31:35 -0500

    Cambios en index.js y validator.js

--- src/index.js
+++ src/index.js
@@ -2,60 +2,34 @@ import validator from './validator.js';
 
 console.log(validator);
 
-let inputNumberCreditCard = document.getElementById ("inputCreditCard");
-    inputNumberCreditCard.oninvalid = function (event) {
-        event.target.setCustomValidity("Por favor, introduce solo números del 0 al 9");
-    };
+let inputNumberCreditCard = document.getElementById("inputCreditCard");
 
-    //Obteniendo el número de la tarjeta de crédito introducida por el usuario en el input.
+//Obteniendo el número de la tarjeta de crédito introducida por el usuario en el input.
 
-let getNumberCreditCard = function(){
-    let listNumbers = document.getElementById("inputCreditCard");
-    let inputValues = listNumbers.value;
+let getNumberCreditCard = function () {
+    let inputValido = inputNumberCreditCard.checkValidity();
+    if (inputValido) {
+
+        let listNumbers = document.getElementById("inputCreditCard");
+        let inputValues = listNumbers.value;
         
-        //Formando un array a partir del número de la tarjeta de crédito obtenida.
+        let result = validator.isValid(inputValues);
+         if (result){
+             alert("Numero de tarjeta Válida")
+         } else {
+             alert ("Por favor introduce una tarjeta válida")
+         }
         
-        let arrayNumbers = [];
-        for(let i = 0; i < 16; i++){
-        arrayNumbers[i] = parseFloat(inputValues[i]);
+        let ocultarNumeros = validator.maskify(inputValues);
+        console.log (ocultarNumeros)
+
+    } else {
+        alert("Por favor, introduce solo números del 0 al 9");
     }
-        for(let i = 0; i < arrayNumbers.length; i++){
-        console.log(arrayNumbers[i]);
-    }
-    
-    //Obteniendo la reversa del array obtenido.
 
-    let reverseArrayNumbers = arrayNumbers.reverse ();
-    console.log (reverseArrayNumbers);
-
-    //Obteniendo un nuevo array a partir de los números que se encuentran en la posición par del array (reverseArrayNumbers)
-
-    let numbersEvenPosition = [];
-
-        for (let i = 0; i < 16; i = i + 2){
-        numbersEvenPosition[i] = reverseArrayNumbers[i];
-        }
-        for (let i = 0; i <numbersEvenPosition.length; i = i + 2){
-        console.log (numbersEvenPosition[i]);
-        }
-
-    //Ahora debemos multiplicar por dos cada número que tenemos en el array numbersEvenPosition (numeros en la posicion par)
-
-    let multiplicationXTwo = [];
-    multiplicationXTwo = numbersEvenPosition.flatMap(x => [x * 2]);
-        
-    console.log (multiplicationXTwo);
-
-    //Si el resultado de la multiplicación de cada dígito es =>10 debemos sumarlos.
-
-
-   
-
-}
+};
 
 //Haciendo que cuando el usuario presione el botón Validar, se ejecute la función getNumberCreditCard para obtener el array.
-let validation = document.getElementById ("Validar");
+let validation = document.getElementById("Validar");
 validation.onclick = getNumberCreditCard;
-
-
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,65 @@
 const validator = {
-  
+
+    isValid: function (inputValues) {
+
+        //Formando un array a partir del número de la tarjeta de crédito obtenida.
+
+        let arrayNumbers = [];
+        for (let i = 0; i < 16; i++) {
+            arrayNumbers[i] = parseFloat(inputValues[i]);
+        }
+        for (let i = 0; i < arrayNumbers.length; i++) {
+            console.log(arrayNumbers[i]);
+        }
+
+        //Obteniendo la reversa del array obtenido.
+
+        let reverseArrayNumbers = arrayNumbers.reverse();
+        console.log(reverseArrayNumbers);
+
+        //Obteniendo un nuevo array donde los números con la posición par se multipliquen por 2.
+
+        let multiplicationXTwo = [];
+
+        for (let i = 0; i < 16; i++) {
+
+            if (i % 2 === 0) {
+                multiplicationXTwo[i] = reverseArrayNumbers[i] * 2;
+
+                // Si el número multiplicado por 2, es mayor a 9 sumamos sus dígitos y creamos un nuevo array con ese valor.
+                if (multiplicationXTwo[i] > 9) {
+                    let numeroDerecho = multiplicationXTwo[i] % 10
+                    let numeroizquierdo = parseInt(multiplicationXTwo[i] / 10)
+                    multiplicationXTwo[i] = numeroizquierdo + numeroDerecho
+                }
+            } else multiplicationXTwo[i] = reverseArrayNumbers[i];
+
+            console.log(multiplicationXTwo[i]);
+        }
+
+        // Sumar todos los números del array y verificar que el resultado sea múltiplo de 10 para que sea válido.
+
+        let sumatoriaTotal = multiplicationXTwo.reduce(function (a, b) {
+            return a + b
+        });
+        console.log(sumatoriaTotal);
+        if (sumatoriaTotal % 10 === 0) {
+            return true
+        } else {
+            return false
+        }
+
+    },
+
+    // Haciendo que luego de que el usuario coloque la tarjeta de crédito a validar, solo se muestren l
+
+    maskify: function (listNumbers) {
+
+        let numerosVisibles = listNumbers.substring(listNumbers.length - 4, listNumbers.length);
+        let numerosNoVisibles = listNumbers.slice(0, listNumbers.length - 4).replace(/[0-9]/g, "#");
+        let unionNumerosVisiblesYNoVisibles = numerosNoVisibles + numerosVisibles;
+        return unionNumerosVisiblesYNoVisibles;
+    }
 };
 
 export default validator;
