31bd8d3275df15988593aec70bfea757110bad55
Nelida Valero <nelidavalero06@gmail.com>
2021-03-23 21:35:27 -0500

    Cambios en todos los archivos

--- src/index.js
+++ src/index.js
@@ -2,34 +2,65 @@ import validator from './validator.js';
 
 console.log(validator);
 
-let inputNumberCreditCard = document.getElementById("inputCreditCard");
+//Cuando cargue la págin sólo se vea la página de inicio.
+document.getElementById("seleccionPostres").style.display = "none";
+document.getElementById("seleccionPasteles").style.display = "none";
+document.getElementById("formPago").style.display = "none";
+
+//Hacer que el BOTÓN Postres solo muestre la página de postres.
+let llamandoPaginaPostres = function () {
+    document.getElementById("containerInicio").style.display = "none";
+    document.getElementById("seleccionPostres").style.display = "inline-block";
+    document.getElementById("seleccionPasteles").style.display = "none";
+    document.getElementById("formPago").style.display = "none";
+};
+(document.getElementById("botonPostres")).onclick = llamandoPaginaPostres;
+
+//Hacer que el BOTÓN Pasteles solo muestre la página de Pasteles.
+let llamandoPaginaPasteles = function () {
+    document.getElementById("containerInicio").style.display = "none";
+    document.getElementById("seleccionPostres").style.display = "none";
+    document.getElementById("seleccionPasteles").style.display = "inline-block";
+    document.getElementById("formPago").style.display = "none";
+};
+(document.getElementById("botonPasteles")).onclick = llamandoPaginaPasteles;
+
+//Mostrando el form de pago
+let llamandoFormPago = function () {
+    document.getElementById("containerInicio").style.display = "none";
+    document.getElementById("seleccionPostres").style.display = "none";
+    document.getElementById("seleccionPasteles").style.display = "none";
+    document.getElementById("formPago").style.display = "flex";
+};
+(document.getElementById ("comprarBrownie")).onclick = llamandoFormPago;
+(document.getElementById ("comprarPie")).onclick = llamandoFormPago;
+(document.getElementById ("comprarRoll")).onclick = llamandoFormPago;
+(document.getElementById ("comprarPastel1")).onclick = llamandoFormPago;
+(document.getElementById ("comprarPastel2")).onclick = llamandoFormPago;
+(document.getElementById ("comprarPastel3")).onclick = llamandoFormPago;
 
 //Obteniendo el número de la tarjeta de crédito introducida por el usuario en el input.
-
+let inputNumberCreditCard = document.getElementById("inputCreditCard");
+let formatoDePagoCompleto = document.getElementById("formPago")
 let getNumberCreditCard = function () {
     let inputValido = inputNumberCreditCard.checkValidity();
     if (inputValido) {
 
-        let listNumbers = document.getElementById("inputCreditCard");
-        let inputValues = listNumbers.value;
-        
-        let result = validator.isValid(inputValues);
-         if (result){
-             alert("Numero de tarjeta Válida")
-         } else {
-             alert ("Por favor introduce una tarjeta válida")
-         }
-        
+        let inputValues = inputNumberCreditCard.value;
         let ocultarNumeros = validator.maskify(inputValues);
-        console.log (ocultarNumeros)
-
+        let result = validator.isValid(inputValues);
+        if (result) {
+            //Mostrando en la pantalla del usuario, el mensaje: tarjeta es Válida para que siga con su compra.
+        formatoDePagoCompleto.innerHTML = "<img src=imagenes/check.jpg>Tarjeta Válida <br> Puedes continuar con tu compra.<br>" + ocultarNumeros + "<button id='ok'>OK</button>";
+        } else {
+            //Mostrando en la pantalla del usuario el mensaje: Tarjeta inválida, por favor verifique sus datos.
+    formatoDePagoCompleto.innerHTML = "<img src=imagenes/error.jpg> Tarjeta Inválida <br> Por favor verifica tus datos.<br>" + ocultarNumeros+ "<button>Volver</button>";
+        }
     } else {
         alert("Por favor, introduce solo números del 0 al 9");
     }
-
 };
 
 //Haciendo que cuando el usuario presione el botón Validar, se ejecute la función getNumberCreditCard para obtener el array.
 let validation = document.getElementById("Validar");
 validation.onclick = getNumberCreditCard;
-
--- src/validator.js
+++ src/validator.js
@@ -3,29 +3,22 @@ const validator = {
     isValid: function (inputValues) {
 
         //Formando un array a partir del número de la tarjeta de crédito obtenida.
-
         let arrayNumbers = [];
-        for (let i = 0; i < 16; i++) {
-            arrayNumbers[i] = parseFloat(inputValues[i]);
+        let indexInputValues = inputValues.length;
+        for (let i = 0; i < indexInputValues; i++) {
+            arrayNumbers[i] = parseInt(inputValues[i]);
         }
-        for (let i = 0; i < arrayNumbers.length; i++) {
-            console.log(arrayNumbers[i]);
-        }
-
+        
         //Obteniendo la reversa del array obtenido.
-
         let reverseArrayNumbers = arrayNumbers.reverse();
-        console.log(reverseArrayNumbers);
-
+        
         //Obteniendo un nuevo array donde los números con la posición par se multipliquen por 2.
-
         let multiplicationXTwo = [];
+        for (let i = 0; i < indexInputValues; i++) {
 
-        for (let i = 0; i < 16; i++) {
-
-            if (i % 2 === 0) {
+            if (i % 2 === 1) {
                 multiplicationXTwo[i] = reverseArrayNumbers[i] * 2;
-
+                
                 // Si el número multiplicado por 2, es mayor a 9 sumamos sus dígitos y creamos un nuevo array con ese valor.
                 if (multiplicationXTwo[i] > 9) {
                     let numeroDerecho = multiplicationXTwo[i] % 10
@@ -33,33 +26,27 @@ const validator = {
                     multiplicationXTwo[i] = numeroizquierdo + numeroDerecho
                 }
             } else multiplicationXTwo[i] = reverseArrayNumbers[i];
-
-            console.log(multiplicationXTwo[i]);
         }
 
         // Sumar todos los números del array y verificar que el resultado sea múltiplo de 10 para que sea válido.
-
         let sumatoriaTotal = multiplicationXTwo.reduce(function (a, b) {
             return a + b
         });
-        console.log(sumatoriaTotal);
+        
         if (sumatoriaTotal % 10 === 0) {
             return true
         } else {
             return false
         }
-
     },
 
     // Haciendo que luego de que el usuario coloque la tarjeta de crédito a validar, solo se muestren l
+    maskify: function (inputValues) {
 
-    maskify: function (listNumbers) {
-
-        let numerosVisibles = listNumbers.substring(listNumbers.length - 4, listNumbers.length);
-        let numerosNoVisibles = listNumbers.slice(0, listNumbers.length - 4).replace(/[0-9]/g, "#");
+        let numerosVisibles = inputValues.substring(inputValues.length - 4, inputValues.length);
+        let numerosNoVisibles = inputValues.slice(0, inputValues.length - 4).replace(/[0-9,a-z,A-Z]/g, "#");
         let unionNumerosVisiblesYNoVisibles = numerosNoVisibles + numerosVisibles;
         return unionNumerosVisiblesYNoVisibles;
     }
-};
-
+}
 export default validator;
